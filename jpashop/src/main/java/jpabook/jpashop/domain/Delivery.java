6a4868e6a6d3f8cf2877af6de2f58b1099d27839
Lucy Oh <56earls@gmail.com>
2024-04-11 01:40:11 +0900

    Section 8. 활용 예제

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,9 +1,6 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Delivery extends BaseEntity {
@@ -12,7 +9,7 @@ public class Delivery extends BaseEntity {
     @GeneratedValue
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     private String city;
