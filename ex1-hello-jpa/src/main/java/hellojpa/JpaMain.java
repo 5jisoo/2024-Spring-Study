b58dd924f98ce60b74d947de334792081c9a9da1
Lucy Oh <56earls@gmail.com>
2024-04-19 16:29:11 +0900

    Section 9. 불변 객체

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,10 +20,16 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("userA");
-            member.setHomeAddress(new Address("cityA", "streetA", "1111"));
+            Address address = new Address("cityA", "streetA", "1111");
+            member.setHomeAddress(address);
             member.setWorkPeriod(new Period());
-
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+//            address.setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
